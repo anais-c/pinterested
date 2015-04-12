72d48c5c6ab044d5dde363a47f36af816ace4984
Anais <anaiskat@gmail.com>
2015-04-12 17:24:06 +0000

    Associated pins with users and authenticated users

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   respond_to :html
 
@@ -13,17 +15,19 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
-    respond_with(@pin)
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end  
   end
 
   def update
@@ -43,5 +47,10 @@ class PinsController < ApplicationController
 
     def pin_params
       params.require(:pin).permit(:description)
+    end
+    
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "No authorized to edit this pin" if @pin.nil?
     end
 end
