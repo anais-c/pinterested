d453f594b64ea95ae42c60f0868d76f617056124
anais <anaiskat@gmail.com>
2015-06-03 09:53:20 +0000

    modified production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'pinterested10.herokuapp.com' }
-end
+  config.action_mailer.default_url_options = { host: 'pinsterested.herokuapp.com' }
+
